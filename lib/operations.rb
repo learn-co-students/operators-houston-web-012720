87e6399d37b4a710b6d685a95c748092cd9ef033
corn-cloud <comowaiye@gmail.com>
2020-01-28 18:23:16 -0600

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,27 @@
 def unsafe?(speed)
+    if speed > 40 && speed < 60 
+        return false
+    end
+
+    if speed > 60
+        return true
+    end
+    if speed < 40
+        return true
+    end
+    
+
 
 end
 
 
 
 def not_safe?(speed)
+    
+    
+    speed < 40 || speed > 60 ? true : false 
+    
+
 	
 end
 	
